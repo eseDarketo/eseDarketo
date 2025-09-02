438adc1125a0330e7c0e868981b6ac1d232617db
isaacMagma <isaac@magmapartners.com>
2025-09-01 20:04:56 -0600

    config theme

--- src/resources/once-ui.config.js
+++ src/resources/once-ui.config.js
@@ -38,10 +38,10 @@ const fonts = {
 
 // default customization applied to the HTML in the main layout.tsx
 const style = {
-  theme: "system", // dark | light | system
+  theme: "dark", // dark | light | system
   neutral: "gray", // sand | gray | slate
-  brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   solid: "contrast", // color | contrast | inverse
   solidStyle: "flat", // flat | plastic
   border: "playful", // rounded | playful | conservative
@@ -66,26 +66,26 @@ const dataStyle = {
 
 const effects = {
   mask: {
-    cursor: false,
+    cursor: true,
     x: 50,
     y: 0,
     radius: 100,
   },
   gradient: {
-    display: false,
+    display: true,
     x: 50,
     y: 0,
     width: 100,
     height: 100,
     tilt: 0,
-    colorStart: "brand-background-strong",
-    colorEnd: "static-transparent",
+    colorStart: "accent-alpha-weak",
+    colorEnd: "accent-alpha-strong",
     opacity: 50,
   },
   dots: {
-    display: true,
+    display: false,
     size: "2",
-    color: "brand-on-background-weak",
+    color: "accent-medium",
     opacity: 40,
   },
   lines: {
